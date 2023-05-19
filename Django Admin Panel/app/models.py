ae156a965e6df32c10cfebe1620fc61765d78b84
Santhosh Kumar <133121635+santhoshmlops@users.noreply.github.com>
2023-05-19 19:43:00 +0530

    Django_Admin_Panel

---
+++ Django Admin Panel/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):
+    Firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=50)
+    Contact = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.Firstname
